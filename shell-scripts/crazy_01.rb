ab4b893e4d465cd3784b114908a6f7190c4a698c
Ashik Salman <ashiksp@gmail.com>
2015-11-04 17:42:12 +0530

    Included Shell scripts repo.

---
+++ shell-scripts/crazy_01.rb
@@ -0,0 +1,7 @@
+#! /bin/bash
+# 
+# Just created file with extension '.rb' and included bash script. Lol :-)
+# As unix is concerned, file extensions are for just name sake.
+#
+# List all the files in a directory
+ls -lah
